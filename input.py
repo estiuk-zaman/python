a2b655edaa2215630b059e7a85d70cb852211240
estiuk <mdestiukuzzaman@gmail.com>
2026-05-21 19:36:40 -0700

    conversation

---
+++ input.py
@@ -0,0 +1,10 @@
+# name = input('Whats your name? ')
+# color = input('Whats your color? ')
+# print(name+' likes '+ color)
+
+
+# birth_year = int(input("Birth Year: "))
+birth_year = input("Birth Year: ")
+age = 2019 - int(birth_year)
+print (type(age))
+print(age)
