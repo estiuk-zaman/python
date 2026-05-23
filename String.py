e1369e35f02bf380ca13f144d7a51f2df1199860
estiuk <mdestiukuzzaman@gmail.com>
2026-05-22 22:05:38 -0700

    String

---
+++ String.py
@@ -0,0 +1,14 @@
+course = "Python's String"
+course01 = 'Python "String"'
+course02= '''
+Hlw istii
+This is a paragraph type string.
+We can send email like this
+
+Thank you
+'''
+# print(course)
+# print(course01)
+# print(course02)
+# print(course[0],course[2],course[4])
+print(course01[0:-8])
