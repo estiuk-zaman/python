06016a9e6f7a5beefb34e079d40cda1606bba930
estiuk <mdestiukuzzaman@gmail.com>
2026-05-26 23:36:05 -0700

    If Statement

---
+++ if_statement.py
@@ -0,0 +1,15 @@
+isHotDay = False;
+isColdDay = False;
+if isHotDay:
+    print("Its a hot day")
+elif isColdDay:
+    print("It's Cold Day.")
+else:
+    print("It's normal Day")
+
+amount=10000
+discount=False
+if discount:
+    print(f"Final Price : ${amount*0.5}")
+else:
+    print(f"Final Price : ${amount}")
