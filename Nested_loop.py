6a818ffe14ca2cf4c575564eb4bb734063b2d58c
estiuk <mdestiukuzzaman@gmail.com>
2026-05-27 03:26:48 -0700

    Nested Loop

---
+++ Nested_loop.py
@@ -0,0 +1,17 @@
+# for index,x in enumerate(range(5,10)):
+#     print(x,"index-",index)
+
+# for x in range(5):
+#     for y in range(4):
+#         print(f"{x},{y}")
+
+# numbers=[5,2,5,2,2]
+# for x in numbers:
+#     print("x"*x)
+
+numbers=[5,2,5,2,2]
+for count in numbers:
+    hehe=""
+    for x_print in range(count):
+        hehe+="x"
+    print(hehe)
