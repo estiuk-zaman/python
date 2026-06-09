c2127699fe75ee6e4babf54f98077a601571d4ac
estiuk <mdestiukuzzaman@gmail.com>
2026-06-08 21:20:48 -0700

    Pandas_01

--- Pandas/app.py
+++ Pandas/app.py
@@ -1,1 +1,5 @@
 import pandas as pd 
+# read csv file as dataframe
+#df = pd.read_csv("D:\Study\Git\python\Pandas\customers-1000.csv")
+df = pd.read_excel("D:\Study\Git\python\Pandas\Project-Management-Sample-Data.xlsx")
+print(df)
